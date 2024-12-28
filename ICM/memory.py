509a3f2aff6525f20f50ebdc9dcea01a02666a97
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-12-28 15:58:21 +0530

    memory for ICM

---
+++ ICM/memory.py
@@ -0,0 +1,24 @@
+class Memory:
+    def __init__(self):
+        self.states_ = []
+        self.pred_states = []
+        self.actions_pred = []
+        self.actions = []
+
+    def remember(self, state_, pred_state, actions, pred_actions):
+        self.actions.append(actions)
+        self.states_.append(state_)
+        self.pred_states.append(pred_state)
+        self.actions_pred.append(pred_actions)
+
+    def clear_memory(self):
+        self.states_ = []
+        self.pred_states = []
+        self.actions_pred = []
+        self.actions = []
+
+    def sample_memory(self):
+        return self.states_, self.pred_states, self.actions, self.actions_pred
+    
+
+    
